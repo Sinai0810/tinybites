5ed353a32999791906a986cb3740e69055a47853
Sinai0810 <sinaicolina08.sc@gmail.com>
2025-08-18 19:29:29 -0400

    Initial Commit

---
+++ Tiny Bites/Tiny_BitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_BitesApp.swift
+//  Tiny Bites
+//
+//  Created by Sinai on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Tiny_BitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tiny BitesTests/Tiny_BitesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_BitesTests.swift
+//  Tiny BitesTests
+//
+//  Created by Sinai on 8/18/25.
+//
+
+import Testing
+@testable import Tiny_Bites
+
+struct Tiny_BitesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
